b3e22c9ec2b6c3770252618a4281a4abb881aeff
McJeffr <mcjeffr@gmail.com>
2020-06-05 16:25:17 +0200

    Initial commit.

---
+++ src/main/kotlin/com/mcjeffr/shotbowplayercountapi/ShotbowPlayercountApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mcjeffr.shotbowplayercountapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShotbowPlayercountApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShotbowPlayercountApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mcjeffr/shotbowplayercountapi/ShotbowPlayercountApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mcjeffr.shotbowplayercountapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShotbowPlayercountApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
